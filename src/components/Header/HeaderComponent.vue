633316399a239888ffef41ecc925b982758bb468
VinayPatel553 <vinay.vp477@gmail.com>
2024-02-23 09:46:07 +0530

    22/2 solved me page error,search functionality

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -3,11 +3,7 @@
 <nav class="navbar navbar-expand-lg  bg-white d-none d-lg-block">
   <div class="container">
     <a class="navbar-brand ms-5"><img src="../../assets/rp-logo.png" alt="" height="50px" width="50px" /></a>
-    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> -->
-    <!-- <div class="collapse navbar-collapse" id="navbarTogglerDemo02"> -->
-
+    
     <div class="input-group" >
   <input type="text" class="form-control" placeholder="Search People,Pages,Groups..." style="background-color: #F5E8E8" v-model="searchQuery" @input="handleSearch">
   <button  v-if="searchResults.length > 0 || showNoDataMessage" class="btn close-button" style="background-color: #F5E8E8" @click="closeAutocompleteContainer">x</button>
@@ -346,15 +342,17 @@
             </div>
 
             <!-- Action icons -->
-            <div class="action-icons action-icons-sidebar ms-5"> 
+            <div class="action-icons action-icons-sidebar container text-center"> 
       <router-link to="/foodfie">
+        <div class="">
         <button class="each-icon-sidebar" v-on:click="handleClick('foodfie')" :class="{active:active=='foodfie'}">
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" :fill="active=='foodfie' ? 'red':'grey'" class="bi bi-phone icon-fill" viewBox="0 0 16 16">
               <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
               <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
             </svg>
-            <span class="icon-text" :style="{color:active=='foodfie'?'red':'grey'}" >FoodFie</span> 
+            <span class="icon-text-sidebar" :style="{color:active=='foodfie'?'red':'grey'}" >FoodFie</span> 
         </button>
+        </div>
       </router-link>
 
       <router-link to="/dinein">
@@ -363,7 +361,7 @@
             <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5m2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5"/>
           </svg>
           <!-- <img src="../../assets/dinein-icon.png" height="30px" width="30px"> -->
-            <span class="icon-text" :style="{color:active=='dinein'?'red':'grey'}" >Dine-In</span> 
+            <span class="icon-text-sidebar" :style="{color:active=='dinein'?'red':'grey'}" >Dine-In</span> 
         </button>
       </router-link>
 
@@ -372,7 +370,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" :fill="active=='notification' ? 'red':'grey'" class="bi bi-bell" viewBox="0 0 16 16">
   <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
 </svg>
-            <span class="icon-text" :style="{color:active=='notification'?'red':'grey'}" >Notifications</span> 
+            <span class="icon-text-sidebar" :style="{color:active=='notification'?'red':'grey'}" >Notifications</span> 
         </button>
       </router-link>
 
@@ -382,7 +380,7 @@
   <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
   <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/>
 </svg>
-            <span class="icon-text" :style="{color:active=='setting'?'red':'grey'}" >Settings</span> 
+            <span class="icon-text-sidebar" :style="{color:active=='setting'?'red':'grey'}" >Settings</span> 
         </button>
       </router-link>
 
@@ -399,7 +397,7 @@
         >
         <img src="../../assets/user-image.jpg" class="rounded" height="30px" width="30px">
         <div class="dropdown-text">
-                <span class="icon-text" :style="{color:active=='me'?'red':'grey'}">Me</span>
+                <span class="icon-text-sidebar" :style="{color:active=='me'?'red':'grey'}">Me</span>
         </div>
         </button>
       </router-link>
@@ -506,6 +504,8 @@ export default{
 .autocomplete-dropdown {
   background-color: #fff;
   border: 1px solid #ccc;
+  max-height: 500px;
+  overflow-y: auto;
 }
 
 .result-item {
@@ -594,14 +594,19 @@ nav{
 .action-icons-sidebar{
     display: flex;
     flex-direction: column;
-    align-items: start;
+    align-items: center;
+  }
+.icon-text-sidebar{
+    font-size: 10px; 
+    font-weight: 600;
+    text-align: left;
   }
 .each-icon-sidebar {
     margin-bottom: 5px; 
 }
 .custom-offcanvas {
-  max-width: 50%; 
-  width: 50%; 
+  max-width: 45%; 
+  width: 45%; 
 }
 .input-group-sidebar{
   width: auto;
--- src/components/Me/UserRequest.vue
+++ src/components/Me/UserRequest.vue
@@ -1,105 +1,69 @@
 <template>
-    <div class="container border bg-white rounded-2">
-      <div class="d-flex mt-2">
-        <span class="text-start text-subheading font-600 grey-color" style="border-bottom: 2px solid #585858;">Request</span>
-        <span class="ms-auto"><input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></span>
-      </div>
-      <div class="row mt-3 ms-2 me-2 mb-3">
-        <div v-for="requestId in currentUser.friendRequests" :key="requestId" class="col-md-6 rounded-3 mt-2 mb-2 p-2" style="border: .5px solid #787878;">
-          <div class="d-flex mt-1 mb-1">
-            <img :src="getUserProfileImg(requestId)" alt="" height="43px" width="43px" class="img border rounded-circle">
-            <div class="text-start mx-2">
-              <span class="fw-medium">{{ getUserName(requestId) }}</span>
-              <br>
-              <span class="fw-light mf">{{ getUserLocation(requestId) }}</span>
-            </div>
-  
-            <div class="ms-auto mt-3">
-              <button class="btn confirm-btn m-1" @click="acceptRequest(requestId)">Accept</button>
-              <button class="btn decline-btn m-1" @click="declineRequest(requestId)">Decline</button>
-            </div>
+  <div class="container border bg-white rounded-2">
+    <div class="d-flex mt-2">
+      <span class="text-start text-subheading font-600 grey-color" style="border-bottom: 2px solid #585858;">Request</span>
+      <span class="ms-auto"><input class="form-control me-2" v-model="searchQuery" type="search" placeholder="Search" aria-label="Search"></span>
+    </div>
+    <div class="row mt-3 ms-2 me-2 mb-3">
+      <div v-for="request in filteredRequests" :key="request.id" class="col-md-6 rounded-3 mt-2 mb-2 p-2" style="border: .5px solid #787878;">
+        <div class="d-flex mt-1 mb-1">
+          <img :src="request.image" alt="" height="43px" width="43px" class="img border rounded-circle">
+          <div class="text-start mx-2">
+            <span class="fw-medium">{{ request.name }}</span>
+            <br>
+            <span class="fw-light mf">{{ request.location }}</span>
+          </div>
+
+          <div class="ms-auto mt-3">
+            <button class="btn confirm-btn m-1" @click="acceptRequest(request.name)">Accept</button>
+            <button class="btn decline-btn m-1" @click="declineRequest(request.name)">Decline</button>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import users from "../users.json";
-  export default {
-    name: 'UserRequest',
-    // data() {
-    //   return {
-    //     requestId:'',
-    //     requests: [
-    //       { id: 1, name: 'Joan Andrews', location: 'Berlin', image: require('../../assets/user2.jpg') },
-    //       { id: 2, name: 'Marie Lucas', location: 'Krakow', image: require('../../assets/user5.jpg') },
-    //       { id: 3, name: 'Barbara Clark', location: 'Wellington', image: require('../../assets/user3.jpg') },
-    //       { id: 4, name: 'Terry Ellis', location: 'Lagos', image: require('../../assets/user4.jpg') },
-    //       { id: 5, name: 'Alan James', location: 'New York', image: require('../../assets/user4.jpg') },
-    //       { id: 6, name: 'Kelly Grand', location: 'Ho Chi Minh', image: require('../../assets/user5.jpg') },
-    //       { id: 7, name: 'Julie Stanley', location: 'Melbourne', image: require('../../assets/user1.jpg') },
-    //       { id: 8, name: 'Joan Pearson', location: 'Vancouver', image: require('../../assets/user2.jpg') },
-    //       { id: 9, name: 'Brandon Cook', location: 'Las Vegas', image: require('../../assets/user3.jpg') },
-    //       { id: 10, name: 'Megan Hart', location: 'Seville', image: require('../../assets/user5.jpg') },
-    //       // Add more request users as needed
-    //     ],
-    //   };
-    // },
-    computed: {
-    currentUser() {
-      return JSON.parse(localStorage.getItem('currentUser'));
-      },
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserRequest',
+  data() {
+    return {
+      searchQuery: "",
+      userId:'',
+      requests: [
+        { id: 1, name: 'Joan Andrews', location: 'Berlin', image: require('../../assets/user2.jpg') },
+        { id: 2, name: 'Marie Lucas', location: 'Krakow', image: require('../../assets/user5.jpg') },
+        { id: 3, name: 'Barbara Clark', location: 'Wellington', image: require('../../assets/user3.jpg') },
+        { id: 4, name: 'Terry Ellis', location: 'Lagos', image: require('../../assets/user4.jpg') },
+        { id: 5, name: 'Alan James', location: 'New York', image: require('../../assets/user4.jpg') },
+        { id: 6, name: 'Kelly Grand', location: 'Ho Chi Minh', image: require('../../assets/user5.jpg') },
+        { id: 7, name: 'Julie Stanley', location: 'Melbourne', image: require('../../assets/user1.jpg') },
+        { id: 8, name: 'Joan Pearson', location: 'Vancouver', image: require('../../assets/user2.jpg') },
+        { id: 9, name: 'Brandon Cook', location: 'Las Vegas', image: require('../../assets/user3.jpg') },
+        { id: 10, name: 'Megan Hart', location: 'Seville', image: require('../../assets/user5.jpg') },
+        // Add more request users as needed
+      ],
+    };
+  },
+  computed: {
+    filteredRequests() {
+      const query = this.searchQuery.toLowerCase();
+      return this.requests.filter(
+        request => request.name.toLowerCase().includes(query) || request.location.toLowerCase().includes(query)
+      );
+    }
+  },
+  methods: {
+    acceptRequest(userId) {
+      alert("You are now following: "+userId)
     },
-    methods: {
-      getUserProfileImg(userId) {
-        const user = users.find((user) => user.id === userId);
-        return user ? user.profile_img : '';
-      },
-      getUserName(userId) {
-        const user = users.find((user) => user.id === userId);
-        return user ? user.name : '';
-      },
-      getUserLocation(userId) {
-        const user = users.find((user) => user.id === userId);
-        return user ? user.location : '';
-      },
-      acceptRequest(userId) {
-          // Logic to accept friend request
-        const currentUser = this.currentUser;
-        const user = users.find((user) => user.id === userId);
-
-        if (user) {
-          // currentUser.friends.push(user.id);
-          currentUser.followers.push(user.id);
-          currentUser.friendRequests = currentUser.friendRequests.filter((id) => id !== user.id);
-          // Update other user's relationships if needed
-
-          // Save updated user data
-          location.reload()
-          localStorage.setItem('currentUser', JSON.stringify(currentUser));
-        }
-      },
-      declineRequest(userId) {
-          // Logic to decline friend request
-        const currentUser = this.currentUser;
-        currentUser.friendRequests = currentUser.friendRequests.filter((id) => id !== userId);
-        // Update other user's relationships if needed
-
-        // Save updated user data
-        location.reload()
-        localStorage.setItem('currentUser', JSON.stringify(currentUser));
-      },
+    declineRequest(userId) {
+      alert("Removed request of: "+userId)
     },
-  };
-  </script>
-  
-  <style scoped>
-  .confirm-btn .decline-btn {
-    border-radius: 8px;
-    padding: 3px 10px 3px 10px;
-  }
-  
+  },
+};
+</script>
 
-  </style>
-  
+<style scoped>
+</style>
